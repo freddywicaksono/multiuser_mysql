3a19083f9bb7f08755c69a0cfa2c5a18a16c962d
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2021-02-13 22:53:50 +0700

    Add files via upload

---
+++ MyMod.vb
@@ -0,0 +1,45 @@
+﻿Imports MySql.Data.MySqlClient
+Module MyMod
+    Public myCommand As New MySqlCommand
+    Public myAdapter As New MySqlDataAdapter
+    Public myData As New DataTable
+    Public DR As MySql.Data.MySqlClient.MySqlDataReader
+    Public SQL As String
+    Public conn As New MySql.Data.MySqlClient.MySqlConnection
+    Public cn As New Connection
+
+   
+    'Tabel User
+    '--------------------------------
+    Public users_baru As Boolean
+    Public oUser As New User
+    '--------------------------------
+
+    Public login_valid As Boolean
+
+
+    Public Sub DBConnect()
+        cn.Host = "localhost"
+        cn.User = "root"
+        cn.Password = ""
+        cn.DatabaseName = "uas2021"
+        cn.Connect()
+    End Sub
+
+    Public Sub DBDisconnect()
+        cn.Disconnect()
+    End Sub
+
+    Public Function getMD5Hash(ByVal strToHash As String) As String
+
+        Dim md5Obj As New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+        bytesToHash = md5Obj.ComputeHash(bytesToHash)
+        Dim strResult As String = ""
+        Dim b As Byte
+        For Each b In bytesToHash
+            strResult += b.ToString("x2")
+        Next
+        Return strResult
+    End Function
+End Module
---
+++ frmLogin.vb
@@ -0,0 +1,28 @@
+﻿Public Class frmLogin
+
+    Private Sub btnLogin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLogin.Click
+        If (txtUsername.Text <> "" And txtPassword.Text <> "") Then
+            oUser.Login(txtUsername.Text, txtPassword.Text)
+            If (login_valid = True) Then
+                If (oUser.rolename = "admin") Then
+                    Form1.Show()
+                    Me.Hide()
+                ElseIf (oUser.rolename = "dosen") Then
+                    Form2.Show()
+                    Me.Hide()
+
+                ElseIf (oUser.rolename = "mahasiswa") Then
+                    Form3.Show()
+                    Me.Hide()
+                Else
+                    MessageBox.Show("Login tidak dikenal")
+
+                End If
+            Else
+
+            End If
+        Else
+            MessageBox.Show("Username dan password wajib diisi!")
+        End If
+    End Sub
+End Class
